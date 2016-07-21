2cdd2f9ec1b9eb55947651fbe6c0cc2f1d83b387
Tong_style <1141632973@qq.com>
2016-07-21 13:17:03 +0800

    turn 4 functions to a function

--- main/main.js
+++ main/main.js
@@ -1,28 +1,11 @@
 'use strict'
 
 function printSum(inputs) {
-  const odds = calculateOdd(inputs);
-  const oddSquares = calculateSquare(odds);
-  const sum = calculateSum(oddSquares);
+  const sum = inputs.filter(input => input%2 === 1)
+      .map(odd => odd * odd)
+      .reduce((prev, cur) => prev + cur);
 
   console.log(sum);
 }
 
-function calculateOdd(inputs) {
-  return inputs.filter(inputs => inputs%2 === 1);
-}
-
-function calculateSquare(odds) {
-  return odds.map(odd => odd * odd);
-}
-
-function calculateSum(oddSquares) {
-  return oddSquares.reduce((prev, cur) => prev + cur);
-}
-
-module.exports = {
-  printSum: printSum,
-  calculateOdd: calculateOdd,
-  calculateSquare: calculateSquare,
-  calculateSum: calculateSum
-}
+exports.printSum = printSum;
